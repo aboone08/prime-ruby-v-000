06e099d5d5209f853c065bbc558fb595a3c23adb
Andranique Boone <andranique.boone@gmail.com>
2019-02-27 23:33:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,4 @@
 # Add  code here!
 def prime?(integer)
-  if integer <= 1
-    return false
-  elsif integer <= 3
-    return true
-  elsif integer%2 == 0 || integer%3 == 0
-    return false
-  end
-  n = 5
-  while (n.sqrt <= integer) do
-    if integer % n == 0 || integer % (n+2) == 0
-      return false
-  end
-    return true
+
 end
