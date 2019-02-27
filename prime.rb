065839e483994517e0a00435f70115eb989b98b6
Andranique Boone <andranique.boone@gmail.com>
2019-02-27 22:20:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
-prime?(integer){
-  if x/2 == 0
-  when x = (1..2)
-    then x+=1
-  end
-}
+def prime?(integer)
+  if integer%2==0
+    return true
+  elsif integer <=2
+    return true
+end
