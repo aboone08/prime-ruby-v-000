52e69d7e8baeac462214e4c1c5fdfa03ced04ce4
Andranique Boone <andranique.boone@gmail.com>
2019-02-28 01:14:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
 def prime?(integer)
- if integer <= 1 && integer%2 == 0
+ if integer = (n+=1) && integer%2 == 0
    return false
- elsif integer/integer == 1 
+ elsif integer/integer == 1
    return true
  end
 end
