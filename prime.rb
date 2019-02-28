1516b91a2f5d0c6438718508ae1516818d4abc74
Andranique Boone <andranique.boone@gmail.com>
2019-02-28 01:37:13 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,9 @@
 def prime?(integer)
  if integer..1 && integer%2 == 0
    return false
- elsif integer >= 1 && ((n * integer) != (n<integer))
+ elsif integer <= 3
    return true
+ elsif integer/integer == 1
+   return false
  end
 end
